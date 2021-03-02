2e1cceb41d9ad5f11f4349b5cb473aa26a305c7c
Ilias Kadimi <ikadimi@SMB.SQLI.COM>
2021-03-02 16:31:14 +0100

    first commit

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
+import { Location } from '@angular/common'
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location) { }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  getHero(): void {
+    this.heroService.getHero(+this.route.snapshot.params.id).subscribe(hero => this.hero = hero);
+  }
+}
